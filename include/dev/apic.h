0088870a8204679e048520eeb5b872e93bf27429
Michael Shaw <spencershawiscool@gmail.com>
2023-02-28 21:43:07 -0500

    apic: added lapic register offset macros

--- include/dev/apic.h
+++ include/dev/apic.h
@@ -9,6 +9,32 @@
 #define MADT_LAPIC_OVRD 5
 #define MADT_X2_LAPIC 9
 
+#define LAPIC_ID 0x20
+#define LAPIC_VERSION 0x30
+#define LAPIC_TPR 0x80
+#define LAPIC_APR 0x90
+#define LAPIC_PPR 0xA0
+#define LAPIC_EOI 0xB0
+#define LAPIC_RRD 0xC0
+#define LAPIC_LDR 0xD0
+#define LAPIC_DFR 0xE0
+#define LAPIC_SIVR 0xF0
+#define LAPIC_ISR 0x100
+#define LAPIC_TMR 0x180
+#define LAPIC_IRR 0x200
+#define LAPIC_ERR 0x280
+#define LAPIC_CMCI 0x2F0
+#define LAPIC_ICR 0x300
+#define LAPIC_LVT_TIMER 0x320
+#define LAPIC_THERMAL 0x330
+#define LAPIC_LVT_PERF_MON_COUNT 0x340
+#define LAPIC_LVT_LINT0 0x350
+#define LAPIC_LVT_LINT1 0x360
+#define LAPIC_LVT_ERR 0x370
+#define LAPIC_INIT_COUNT 0x380
+#define LAPIC_CUR_COUNT 0x390
+#define LAPIC_DIVIDE_CONFIG 0x3E0
+
 #ifndef __ASM__
 
 #include <kernel/common.h>
