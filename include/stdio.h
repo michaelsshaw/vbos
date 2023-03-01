93fe0a1d2f5d20d79872ab42c53849a0b68bf760
Michael Shaw <spencershawiscool@gmail.com>
2023-02-28 20:04:14 -0500

    removed temporary printf_error

--- include/stdio.h
+++ include/stdio.h
@@ -1,8 +1,6 @@
 #ifndef _STDIO_H_
 #define _STDIO_H_
 
-#define printf_error(_fmt, ...) printf("[ERROR]: " _fmt, __VA_ARGS__);
-
 int printf(const char *restrict fmt, ...);
 
 #endif /* _SDTIO_H_ */
