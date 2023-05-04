ce430f490dca67814f0ab1e44d84a8859edff00e
Michael Shaw <spencershawiscool@gmail.com>
2023-05-04 16:25:07 -0400

    pci: change messages to debug prints
    this also disables interrupts during ISRs

--- dev/pci.c
+++ dev/pci.c
@@ -1,6 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 #include <kernel/pio.h>
+#include <kernel/slab.h>
 #include <dev/pci.h>
+
+#define MAX_PCI_DEVICES 256
+
+static struct pci_device *pci_devices;
+static uint8_t pci_device_count;
 
 uint16_t pci_config_read_word(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset)
 {
@@ -18,25 +24,43 @@ uint16_t pci_config_read_word(uint8_t bus, uint8_t slot, uint8_t func, uint8_t o
 	return tmp;
 }
 
-static void pci_print_bus(uint8_t bus)
+static void pci_scan_bus(uint8_t bus)
 {
 	for (uint8_t slot = 0; slot < 32; slot++) {
 		uint16_t vendor_id = pci_config_read_word(bus, slot, 0, 0);
-		if (vendor_id == 0xFFFF)
-			continue;
-
+		uint16_t device_id = pci_config_read_word(bus, slot, 0, 2);
 		uint8_t class = pci_config_read_word(bus, slot, 0, 0xA);
-		if(class == 0xFF)
-			continue;
-
 		uint8_t subclass = (pci_config_read_word(bus, slot, 0, 0xA) >> 8);
 
-		printf(LOG_INFO "PCI device [bus:%xh  slot:%xh]: class=%xh sub=%xh\n", bus, slot, class, subclass);
+		if (vendor_id == 0xFFFF || class == 0xFF)
+			continue;
+
+		pci_devices[pci_device_count].bus = bus;
+		pci_devices[pci_device_count].slot = slot;
+		pci_devices[pci_device_count].vendor_id = vendor_id;
+		pci_devices[pci_device_count].device_id = device_id;
+		pci_devices[pci_device_count].class = class;
+		pci_devices[pci_device_count].subclass = subclass;
+
+		pci_device_count++;
 	}
 }
 
 void pci_init()
 {
+	pci_devices = kzalloc(sizeof(struct pci_device) * MAX_PCI_DEVICES);
+	pci_device_count = 0;
+
 	for (int bus = 0; bus < 256; bus++)
-		pci_print_bus(bus);
+		pci_scan_bus(bus);
+
+#ifdef KDEBUG
+	/* Print PCI devices */
+	for (int i = 0; i < pci_device_count; i++) {
+		struct pci_device *dev = &pci_devices[i];
+		const char fmt[] = LOG_DEBUG "PCI device BUS[%d:%d] CLASS[%d:%d] vendor_id=%xh device_id=%xh\n";
+
+		printf(fmt, dev->bus, dev->slot, dev->class, dev->subclass, dev->vendor_id, dev->device_id);
+	}
+#endif /* KDEBUG */
 }
--- include/dev/pci.h
+++ include/dev/pci.h
@@ -7,6 +7,18 @@
 #define PCIPM_CONFIG_ADDRESS 0xCF8
 #define PCIPM_CONFIG_DATA 0xCFC
 
+struct pci_device {
+	uint8_t bus;
+	uint8_t slot;
+
+	uint8_t class;
+	uint8_t subclass;
+
+	uint16_t vendor_id;
+	uint16_t device_id;
+
+};
+
 void pci_init();
 uint16_t pci_config_read_word(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
 
