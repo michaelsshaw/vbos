746c907a43c0d8afd5910014c29d684da9b8ff2a
Michael Shaw <spencershawiscool@gmail.com>
2023-10-18 20:58:03 -0400

    string: add strncpy

--- include/string.h
+++ include/string.h
@@ -11,8 +11,10 @@
 
 void *memcpy(void *dest, const void *src, size_t num);
 int memcmp(const void *aa, const void *bb, size_t num);
+int strcmp(const char *a, const char *b);
 void *memset(void *str, int c, size_t n);
 char *strcpy(char *dest, const char *src);
+char *strncpy(char *dest, const char *src, size_t n);
 char *strtok(char *str, const char *delim);
 char **strsplit(const char *str, char delim, int *num);
 size_t strlen(const char *c);
--- lib/string.c
+++ lib/string.c
@@ -35,6 +35,16 @@ char *strcpy(char *dest, const char *src)
 	return dest;
 }
 
+char *strncpy(char *dest, const char *src, size_t n)
+{
+	for (size_t i = 0; i < n; i++) {
+		dest[i] = src[i];
+		if (!src[i])
+			break;
+	}
+	return dest;
+}
+
 size_t strlen(const char *c)
 {
 	const char *b = c;
