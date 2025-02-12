15c0de6de8cd51125588e2189a32d321cc081ca1
Michael Shaw <spencershawiscool@gmail.com>
2025-02-11 21:34:08 -0500

    lock: add mutex
    Signed-off-by: Michael Shaw <spencershawiscool@gmail.com>

--- include/kernel/lock.h
+++ include/kernel/lock.h
@@ -3,9 +3,13 @@
 #define _LOCK_H_
 
 typedef int spinlock_t;
+typedef spinlock_t mtx_t;
 
 int atomic_cmpxchg(volatile int *ptr, int cmpval, int newval);
 void spinlock_acquire(spinlock_t *lock);
 void spinlock_release(spinlock_t *lock);
+
+void mtx_acquire(mtx_t *mtx);
+void mtx_release(mtx_t *mtx);
 
 #endif /* _LOCK_H_ */
--- lib/lock.c
+++ lib/lock.c
@@ -1,13 +1,31 @@
 #include <kernel/common.h>
 #include <kernel/lock.h>
 
+#include <kernel/proc.h>
+
+static uint64_t spinlock_attempt_acquire(spinlock_t *lock)
+{
+	return atomic_cmpxchg(lock, 0, 1);
+}
+
 void spinlock_acquire(spinlock_t *lock)
 {
-	while (atomic_cmpxchg(lock, 0, 1))
+	while (spinlock_attempt_acquire(lock))
 		;
 }
 
 void spinlock_release(spinlock_t *lock)
 {
 	atomic_cmpxchg(lock, 1, 0);
+}
+
+void mtx_acquire(mtx_t *mtx)
+{
+	while (spinlock_attempt_acquire(mtx))
+		sswtch();
+}
+
+void mtx_release(mtx_t *mtx)
+{
+	spinlock_release(mtx);
 }
